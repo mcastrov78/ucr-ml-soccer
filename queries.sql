5e01cf7b9710458528571832c2d5987eb9f21452
Mauricio Castro <mauricio.castro@avantica.net>
2020-05-05 16:35:45 -0600

    Some refactoring for CSV generation

--- queries.sql
+++ queries.sql
@@ -3,30 +3,29 @@ select * from country -- EPL 1729
 
 select * from league -- EPL 1729
 
-select count(*) from match
+select count(*) from match -- 25,979
 select * from match
 
-select count(*) from player
+select count(*) from player -- 11,060
 select * from player
-select * from player where player_name like "%ronaldo%" --30893
+select * from player where player_name like "%ronaldo%" -- id=1995 / player_api_id=30893
 
-select count(*) from player_attributes
+select count(*) from player_attributes -- 183,978
 select * from player_attributes
-select * from player_attributes where player_api_id = 30894
+select * from player_attributes where player_api_id = 30894 order by date
 
-select count(*) from team
+select count(*) from team -- 299
 select * from team
-select * from team where team_long_name like "%juventus%" --9885
+select * from team where team_long_name like "%juventus%" -- id=20522 / team_api_id=9885 / team_fifa_api_id=45
 
-select count(*) from team_attributes
+select count(*) from team_attributes --1,458
 select * from team_attributes
 select * from team_attributes where team_api_id = 9885
 
 
 -- EPL
-select count(*) from match where country_id = 1729 -- 3040 games
+select count(*) from match where country_id = 1729 -- 3040 games (8 * 380)
 select * from match where country_id = 1729 order by season
-
 
 select count(*) from match where country_id = 1729 and season = "2008/2009" -- 380
 select count(*) from match where country_id = 1729 and season = "2009/2010" -- 380
@@ -39,13 +38,61 @@ select count(*) from match where country_id = 1729 and season = "2015/2016" -- 3
 
 
 -- POSSESSION
-select count(*) from match where country_id = 1729 and possession is not null and length(trim(possession)) > 0 -- 3040 games
-select possession from match where country_id = 1729 -- 3040 games
+select count(*) from match where country_id = 1729 and (possession is not null and length(trim(possession)) > 0) -- 3040 games
 select possession from match where country_id = 1729 -- 3040 games
 select id, country_id, season, possession from match where country_id = 1729 limit 100
 
 select count(*) from match where country_id = 1729 and possession like "%<elapsed>90</elapsed>%" --2244
-select possession from match where country_id = 1729 and possession like "%<elapsed>90</elapsed>%"
+select id, country_id, season, possession from match where country_id = 1729 and possession like "%<elapsed>90</elapsed>%"
 
 select count(*) from match where country_id = 1729 and possession NOT like "%<elapsed>90</elapsed>%" --796
-select possession from match where country_id = 1729 and possession NOT like "%<elapsed>90</elapsed>%" --796
+select id, country_id, season, possession from match where country_id = 1729 and possession NOT like "%<elapsed>90</elapsed>%" --796
+
+
+-- GAME
+select * from team where team_long_name like "%leicester%" -- team_api_id=8197
+select * from team where team_long_name like "%swansea%" -- team_api_id=10003
+select * from match where league_id = 1729 and home_team_api_id = 8197
+select * from match where league_id = 1729 and away_team_api_id = 8197
+
+-- LEI games in 2018/2019
+select m.id, m.league_id, m.season, m.stage, m.date, m.match_api_id, m.home_team_api_id, m.away_team_api_id, t1.team_long_name as HOME, t2.team_long_name as AWAY, m.home_team_goal, m.away_team_goal
+from match as m, team as t1, team as t2
+where m.league_id = 1729 and m.season = "2015/2016"
+and (m.home_team_api_id = 8197 or m.away_team_api_id = 8197)
+and (m.home_team_api_id = t1.team_api_id)
+and (m.away_team_api_id = t2.team_api_id)
+order by date
+
+
+-- MATCH SWA vs LEI
+select * from match where match_api_id = 1989053
+select id from match where league_id = 1729 and season = "2015/2016" and (home_team_api_id = 8197 or away_team_api_id = 8197)
+
+
+-- PLAYERS SWA vs LEI
+select p.player_api_id, p.player_name, m.match_api_id, m.home_team_api_id as team_api_id, t.team_long_name from player as p, match as m, team as t
+where m.match_api_id = 1989053
+and p.player_api_id in (m.home_player_1, m.home_player_2, m.home_player_3, m.home_player_4, m.home_player_5, m.home_player_6, m.home_player_7, m.home_player_8, m.home_player_9, m.home_player_10, m.home_player_11)
+and m.home_team_api_id = t.team_api_id
+UNION
+select p.player_api_id, p.player_name, m.match_api_id, m.away_team_api_id as team_api_id, t.team_long_name from player as p, match as m, team as t
+where m.match_api_id = 1989053
+and p.player_api_id in (m.away_player_1, m.away_player_2, m.away_player_3, m.away_player_4, m.away_player_5, m.away_player_6, m.away_player_7, m.away_player_8, m.away_player_9, m.away_player_10, m.away_player_11)
+and m.away_team_api_id = t.team_api_id
+ORDER BY team_api_id, player_name
+
+
+-- OTHER FEATURES
+select * from match where country_id = 1729 -- 3040 games
+select count(*) from match where country_id = 1729 and (possession is not null and length(trim(possession)) > 0) -- 3040 games
+select count(*) from match where country_id = 1729 and (goal is not null and length(trim(goal)) > 0) -- 3040 games
+select count(*) from match where country_id = 1729 and (shoton is not null and length(trim(shoton)) > 0) -- 3040 games
+select count(*) from match where country_id = 1729 and (shotoff is not null and length(trim(shotoff)) > 0) -- 3040 games
+select count(*) from match where country_id = 1729 and (corner is not null and length(trim(corner)) > 0) -- 3040 games
+
+-- https://www.footballcritic.com/premier-league-leicester-city-fc-swansea-city-afc/match-stats/508454
+select id, league_id, season, stage, date, match_api_id, home_team_api_id, away_team_api_id, goal from match where match_api_id = 1989053
+select id, league_id, season, stage, date, match_api_id, home_team_api_id, away_team_api_id, shoton from match where match_api_id = 1989053
+select id, league_id, season, stage, date, match_api_id, home_team_api_id, away_team_api_id, shotoff from match where match_api_id = 1989053
+select id, league_id, season, stage, date, match_api_id, home_team_api_id, away_team_api_id, corner from match where match_api_id = 1989053
--- soccer.py
+++ soccer.py
@@ -2,36 +2,48 @@ import sqlite3
 import pandas as pd
 import xml.etree.ElementTree as ET
 
-conn = sqlite3.connect("soccer.sqlite")
-soccer_df = pd.read_sql("select * from match where country_id = 1729 limit 100", conn)
-conn.close()
 
-print("soccer_df: %s" % soccer_df)
-print("len(soccer_df): %s " % len(soccer_df))
+def get_dataframe(league):
+    conn = sqlite3.connect("soccer.sqlite")
+    soccer_df = pd.read_sql("select * from match where league_id = {}".format(league), conn)
+    conn.close()
+    return soccer_df
 
-possession_elapsed_array = [0] * len(soccer_df)
-possession_home_array = [0] * len(soccer_df)
-possession_away_array = [0] * len(soccer_df)
 
-for row_number, row in enumerate(soccer_df["possession"]):
-    possession_tree = ET.fromstring(row)
-    number_of_values = len(list(possession_tree))
+def save_data_to_csv(soccer_df):
+    print("len(soccer_df): %s " % len(soccer_df))
+    print("soccer_df: %s " % soccer_df)
+    possession_elapsed_array = [0] * len(soccer_df)
+    possession_home_array = [0] * len(soccer_df)
+    possession_away_array = [0] * len(soccer_df)
 
-    for child_number, child in enumerate(possession_tree):
-        if child_number == number_of_values - 1:
-            print("elapsed: %s - homepos: %s - awaypos: %s" %
-                  (child.find("elapsed").text, child.find("homepos").text, child.find("awaypos").text))
-            possession_elapsed_array[row_number] = child.find("elapsed").text
-            possession_home_array[row_number] = child.find("homepos").text
-            possession_away_array[row_number] = child.find("awaypos").text
+    for row_number, row in enumerate(soccer_df["possession"]):
+        possession_tree = ET.fromstring(row)
+        number_of_values = len(list(possession_tree))
 
-soccer_df["possession_elapsed"] = possession_elapsed_array
-soccer_df["possession_home"] = possession_home_array
-soccer_df["possession_away"] = possession_away_array
+        for child_number, child in enumerate(possession_tree):
+            if child_number == number_of_values - 1:
+                if child.find("elapsed") is not None and child.find("homepos") is not None and child.find("awaypos") is not None:
+                    print("row: %s - elapsed: %s - homepos: %s - awaypos: %s" %
+                          (row_number, child.find("elapsed").text, child.find("homepos").text, child.find("awaypos").text))
+                    possession_elapsed_array[row_number] = child.find("elapsed").text
+                    possession_home_array[row_number] = child.find("homepos").text
+                    possession_away_array[row_number] = child.find("awaypos").text
 
-soccer_df.to_csv("soccer.csv", index=False,
-                 columns=["id", "country_id", "league_id", "season", "stage", "date", "match_api_id",
-                          "home_team_api_id", "away_team_api_id", "home_team_goal", "away_team_goal",
-                          "possession_elapsed", "possession_home", "possession_away"])
+    soccer_df["possession_elapsed"] = possession_elapsed_array
+    soccer_df["possession_home"] = possession_home_array
+    soccer_df["possession_away"] = possession_away_array
 
-print(soccer_df)
+    soccer_df.to_csv("soccer.csv", index=False,
+                     columns=["id", "country_id", "league_id", "season", "stage", "date", "match_api_id",
+                              "home_team_api_id", "away_team_api_id", "home_team_goal", "away_team_goal",
+                              "possession_elapsed", "possession_home", "possession_away"])
+
+
+def main():
+    soccer_df = get_dataframe(1729)
+    save_data_to_csv(soccer_df)
+
+
+if __name__ == "__main__":
+    main()
